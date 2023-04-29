2d01d27f7edfdba0e7a0ef27fd92c005c3e8f5f8
Edson Sequeira <127318228+EdsonBJJ@users.noreply.github.com>
2023-04-28 21:48:39 -0300

    Conclusão do projeto

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom (idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  }
+  else {
+    alert('Elemento não encontrado');
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -13,6 +20,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
   tecla.onclick = function () {
       tocaSom(idAudio);
+  }
+
+  tecla.onkeydown = function (evento) {
+    if(evento.code === "Enter" || evento.code === "Space"){
+    tecla.classList.add('ativa');
+    }
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
   }
 
 }
